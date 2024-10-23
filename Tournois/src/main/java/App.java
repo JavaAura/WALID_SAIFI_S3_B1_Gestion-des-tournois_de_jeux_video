2282b1f076141b16984b90789113db3b779f009f
Walid Saifi <saifiwalid21@gmail.com>
2024-10-23 11:27:26 +0100

    modification sur configaration

---
+++ Tournois/src/main/java/App.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+public class App {
+
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+
+        logger.info("test logger");
+        
+    }
+}
