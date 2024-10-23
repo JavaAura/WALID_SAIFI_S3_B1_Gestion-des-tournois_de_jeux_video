9cf7eb5f6b61e103cbbeebc5b155f4a7570bc1d8
Walid Saifi <saifiwalid21@gmail.com>
2024-10-23 14:11:46 +0100

    ajouter h2

--- Tournois/src/main/java/App.java
+++ Tournois/src/main/java/App.java
@@ -1,13 +1,21 @@
 import java.util.logging.Logger;
+import org.h2.tools.Server;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-
 
     private static final Logger logger = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
 
-        logger.info("test logger");
-        
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Server h2WebServer = context.getBean("h2WebServer", Server.class);
+        String h2ConsoleUrl = h2WebServer.getURL();
+
+        System.out.println("H2 Console URL: " + h2ConsoleUrl);
+
+        logger.info("H2 Console URL: " + h2ConsoleUrl);
     }
 }
